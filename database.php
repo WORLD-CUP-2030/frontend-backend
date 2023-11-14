b670beac9ba6ea408fc9cea8856ba3d415795f50
Asmae Hariti <haritiasmaa74@gmail.com>
2023-11-15 00:05:44 +0100

    connectin to the db && bringing data and showing it

--- database.php
+++ database.php
@@ -1,13 +1,14 @@
 <?php
 $servername = "localhost";
-$username = "username";
-$password = "password";
-
-$conn = mysqli_connect($servername, $username, $password);
+$username = "root";
+$password = "";
+$dbName = "worldcup";
+// Create connection
+$conn = mysqli_connect($servername, $username, $password,$dbName);
 
 // Check connection
 if (!$conn) {
   die("Connection failed: " . mysqli_connect_error());
 }
-echo "Connected successfully";
+echo "Connected successfully ";
 ?>
--- index.php
+++ index.php
@@ -1,12 +1,45 @@
+<?php
+// Database connection
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "worldcup";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// $sql = "SELECT * FROM team ";
+// $result = $conn->query($sql);
+
+$sql = "SELECT Name, GROUP_CONCAT(Country) AS Countries, Continent FROM team GROUP BY Name";
+$result = $conn->query($sql);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Display Teams</title>
 </head>
 <body>
-    <h1>hello</h1>
-  
+    <h2>Teams Data</h2>
+
+    <?php
+    // Check if there are rows in the result
+    if ($result->num_rows > 0) {
+        // Output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "<p><strong>" . $row["Name"] . ":</strong> " . $row["Countries"] . " - " . $row["Continent"] . "</p>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+    $conn->close();
+    ?>
 </body>
 </html>
