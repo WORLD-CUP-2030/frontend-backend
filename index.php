18f9ff388e3a6fae42b5f8a53388646bced7e29f
Asmae Hariti <haritiasmaa74@gmail.com>
2023-11-15 11:42:26 +0100

    adding tailwind css + displaying coutries in columns

--- index.php
+++ index.php
@@ -16,7 +16,10 @@ if ($conn->connect_error) {
 
 $sql = "SELECT Name, GROUP_CONCAT(Country) AS Countries, Continent FROM team GROUP BY Name";
 $result = $conn->query($sql);
+
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
@@ -24,16 +27,33 @@ $result = $conn->query($sql);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Display Teams</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 </head>
 <body>
     <h2>Teams Data</h2>
 
     <?php
-    // Check if there are rows in the result
     if ($result->num_rows > 0) {
         // Output data of each row
         while ($row = $result->fetch_assoc()) {
-            echo "<p><strong>" . $row["Name"] . ":</strong> " . $row["Countries"] . " - " . $row["Continent"] . "</p>";
+            // Explode the "Countries" field into an array
+            $countriesArray = explode(",", $row["Countries"]);
+
+            echo "
+            <div class='flex flex-col'>
+                <p><strong>" . $row["Name"] . ":</strong></p>
+
+                <div class='flex flex-col'>";
+            
+            // Iterate over each country and display
+            foreach ($countriesArray as $country) {
+                echo "<p>" . trim($country) . "</p>";
+            }
+
+            echo "
+                </div>
+                <p><strong>Continent:</strong> " . $row["Continent"] . "</p>
+            </div>";
         }
     } else {
         echo "0 results";
@@ -41,5 +61,7 @@ $result = $conn->query($sql);
 
     $conn->close();
     ?>
+
+    <button class="text-red-400">See more</button>
 </body>
 </html>
